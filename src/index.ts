3440e557cc7645164e51bdb51d54538312f8a8ad
jihchi <achi@987.tw>
2021-05-30 19:37:46 +0800

    Launch ReScript at buildStart stage

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
 import { Plugin } from 'vite';
-import { debug as Debug } from 'debug';
+import execa from 'execa';
+import npmRunPath from 'npm-run-path';
+import chalk from 'chalk';
 
-export interface VitePluginReScript {}
-
-const debug = Debug('@jihchi/vite-plugin-rescript');
+export interface VitePluginReScript {
+  cmd?: string;
+}
 
 export default function createReScriptPlugin(
   options?: VitePluginReScript
 ): Plugin {
-  debug('options:', options);
+  const cmd = options?.cmd ?? 'rescript build -with-deps -w';
   return {
     name: 'rollup-plugin-rescript',
     async buildStart() {
-      debug('buildStart');
+      const { stdout, stderr } = execa.command(cmd, {
+        env: npmRunPath.env(),
+        extendEnv: true,
+        shell: true,
+        windowsHide: false,
+        cwd: process.cwd(),
+      });
+      function dataListener(chunk: any) {
+        console.log(
+          chalk.cyan('[@jihchi/vite-plugin-rescript]'),
+          chunk.toString()
+        );
+      }
+      stdout && stdout.on('data', dataListener);
+      stderr && stderr.on('data', dataListener);
     },
   };
 }
