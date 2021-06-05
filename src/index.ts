1af16eb8d1c583b3d803d6f452f9fe9876ec6325
jihchi <achi@987.tw>
2021-06-05 10:19:42 +0800

    Add "rescript" as a peer dependency

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,28 @@ export interface VitePluginReScript {
   cmd?: string;
 }
 
+const logPrefix = chalk.cyan('[@jihchi/vite-plugin-rescript]');
+
 export default function createReScriptPlugin(
   options?: VitePluginReScript
 ): Plugin {
   const cmd = options?.cmd ?? 'rescript build -with-deps -w';
   return {
-    name: 'rollup-plugin-rescript',
+    name: '@jihchi/vite-plugin-rescript',
     async buildStart() {
-      const { stdout, stderr } = execa.command(cmd, {
+      const subprocess = execa.command(cmd, {
         env: npmRunPath.env(),
         extendEnv: true,
         shell: true,
         windowsHide: false,
         cwd: process.cwd(),
       });
+
       function dataListener(chunk: any) {
-        console.log(
-          chalk.cyan('[@jihchi/vite-plugin-rescript]'),
-          chunk.toString()
-        );
+        console.log(logPrefix, chunk.toString().trimEnd());
       }
+
+      const { stdout, stderr } = subprocess;
       stdout && stdout.on('data', dataListener);
       stderr && stderr.on('data', dataListener);
     },
