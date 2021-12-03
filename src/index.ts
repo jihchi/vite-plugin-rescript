73414402d2aaddd054eaf3a5b454acb7574d1df5
Bjørn Hoffmann <bhof@tv2.dk>
2021-12-03 15:36:49 +0100

    Add hmr overlay support for compiler errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { readFile } from 'fs';
 import { Plugin } from 'vite';
 import execa from 'execa';
+import glob from 'glob';
 import npmRunPath from 'npm-run-path';
 import chalk from 'chalk';
+import parseErrorLog from './overlay';
 
 const logPrefix = chalk.cyan('[@jihchi/vite-plugin-rescript]');
 
@@ -22,6 +25,7 @@ async function launchReScript(watch: boolean) {
 
   function dataListener(chunk: any) {
     const output = chunk.toString().trimEnd();
+    // eslint-disable-next-line no-console
     console.log(logPrefix, output);
     if (watch && output.includes('>>>> Finish compiling')) {
       compileOnce(true);
@@ -39,8 +43,6 @@ async function launchReScript(watch: boolean) {
   } else {
     await result;
   }
-
-  return;
 }
 
 export default function createReScriptPlugin(): Plugin {
@@ -53,6 +55,45 @@ export default function createReScriptPlugin(): Plugin {
 
       if (needReScript) {
         await launchReScript(command === 'serve' || Boolean(build.watch));
+      }
+    },
+    config: () => ({
+      server: {
+        watch: {
+          // Only watch for changes to the actual compiled js files, except the
+          // ".compiler.log" file which is needed for showing the error overlay.
+          // JS is not outputted when the compiler errors, but the rescript files
+          // would still trigger an update. That is why they are ignored here.
+          ignored: [
+            '**/*.res',
+            '**/*.resi',
+            '**/lib/bs',
+            '!**/lib/bs/.compiler.log',
+          ],
+        },
+      },
+    }),
+    configureServer(server) {
+      // Manually find and parse log file after server start since
+      // initial compilation does not trigger handleHotUpdate.
+      glob('**/lib/bs/.compiler.log', {}, (globError, files) => {
+        if (!globError && files.length > 0) {
+          const [logPath] = files;
+          readFile(logPath, (readFileError, data) => {
+            if (!readFileError && data) {
+              const log = data.toString();
+              const err = parseErrorLog(log);
+              if (err) server.ws.send({ type: 'error', err });
+            }
+          });
+        }
+      });
+    },
+    async handleHotUpdate({ file, read, server }) {
+      if (file.endsWith('.compiler.log')) {
+        const log = await read();
+        const err = parseErrorLog(log);
+        if (err) server.ws.send({ type: 'error', err });
       }
     },
   };
---
+++ src/overlay.ts
@@ -0,0 +1,101 @@
+import { EOL } from 'os';
+
+// https://github.com/rescript-lang/rescript-vscode/blob/7ab2d231f91fee2f93cbf6cae1b38f94c06a58c1/server/src/utils.ts#L288
+const fileAndRangeRegex = /(.+):(\d+):(\d+)(-(\d+)(:(\d+))?)?$/;
+
+// https://github.com/rescript-lang/rescript-vscode/blob/7ab2d231f91fee2f93cbf6cae1b38f94c06a58c1/server/src/utils.ts#L433
+const codeRegex = /^  +([0-9]+| +|\.) (│|┆)/;
+
+// Compiler can treat warnings as hard errors based on `warnings` object in bsconfig.json
+const warningAsErrorRegex = /Warning number \d+ \(configured as error\)/;
+
+// Returns true if the line indicates the start of an error block
+function isErrorLine(line: string) {
+  if (line.startsWith("  We've found a bug for you!")) return true;
+  if (line.startsWith('  Syntax error!')) return true;
+  if (warningAsErrorRegex.test(line)) return true;
+  return false;
+}
+
+export default function parseErrorLog(log: string) {
+  // Split by line endings and remove empty lines
+  const lines = log.split(EOL).filter(Boolean);
+
+  // Optimization; only parse log when compiler is done
+  if (lines[lines.length - 1].startsWith('#Done(')) {
+    let foundError = false;
+    let path = '';
+    let startLine = 0;
+
+    // There can be additional messages such as hints
+    const messages = [];
+
+    // Avoid trimming the indentation in vite overlay by adding a horizontal ruler
+    // https://github.com/vitejs/vite/blob/96591bf9989529de839ba89958755eafe4c445ae/packages/vite/src/client/overlay.ts#L144
+    const frame = ['—'.repeat(80)];
+
+    // Parse the log file line by line. It might seem weird having to resort to log parsing,
+    // but this is actually how the official rescript language server works:
+    // https://github.com/rescript-lang/rescript-vscode/blob/7ab2d231f91fee2f93cbf6cae1b38f94c06a58c1/server/src/utils.ts#L368
+    for (let i = 0; i < lines.length; i += 1) {
+      const line = lines[i];
+      if (isErrorLine(line)) {
+        // An error has already been found, but only one can be handled. Do the sane thing
+        // here by considering the parsing done. It might be possible to combine the multiple
+        // error messages into one, but that does not seem worth it at the moment.
+        if (foundError) break;
+        foundError = true;
+
+        // Optimization; the next line is always the file + range, which means it
+        // can be parsed now and the next iteration (line) can be skipped.
+        path = lines[i + 1].trim();
+
+        // Extract the start line
+        const match = path.match(fileAndRangeRegex);
+        if (match) startLine = parseInt(match[2], 10);
+
+        // Skip the next line since it was handled here
+        i += 1;
+      } else if (!foundError) {
+        // Only run below checks once an error has been found
+      } else if (line.startsWith('  Warning number ')) {
+        // Reached the end of the error
+        break;
+      } else if (line.startsWith('#Done(')) {
+        // Reached the end of the log file
+        break;
+      } else {
+        // This can now only be code lines or messages
+        const match = line.match(codeRegex);
+        if (match) {
+          // Replace strange vertical bars with regular ones in order to match
+          // the code frame regex defined in the vite overlay file:
+          // https://github.com/vitejs/vite/blob/96591bf9989529de839ba89958755eafe4c445ae/packages/vite/src/client/overlay.ts#L116
+          let codeFrameLine = line.replace('┆', '|').replace('│', '|');
+
+          // Since the red color indicator is lost when parsing the log file,
+          // this adds a pointer (`>`) to the line where the error starts.
+          if (parseInt(match[1], 10) === startLine) {
+            codeFrameLine = `> ${codeFrameLine.substring(2)}`;
+          }
+
+          frame.push(codeFrameLine);
+        } else if (line.startsWith('  ')) {
+          // It has to be a message by now
+          messages.push(line.trim());
+        }
+      }
+    }
+
+    if (foundError) {
+      return {
+        message: messages.join('\n'),
+        frame: `${frame.join('\n')}`,
+        stack: '',
+        id: path,
+      };
+    }
+  }
+
+  return null;
+}
